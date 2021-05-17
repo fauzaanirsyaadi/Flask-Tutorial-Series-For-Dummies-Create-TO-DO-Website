7eb2eda6b5fd104fb5b8a998f2317e9790d659cf
fauzaanirsyaadi <56173199+fauzaanirsyaadi@users.noreply.github.com>
2021-05-17 11:13:41 +0700

    HTTP Methods (GET POST) & Retrieving Form Data In Flask

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-  return'<h1>Hello world</h1>'
+  request_method = request.method
+  if request.method=="POST":
+    print("=====")
+    print(request.form)
+    print("=====")
+    first_name=request.form('first_name')
+    return redirect(url_for('name', first_name=first_name))
+  return render_template('hello.html', request_method=request_method)
 
+@app.route('/name/<string:first_name>')
+def name(first_name):
+  return f'{first_name}'
 if __name__ == '__main__':#ketika file ini dibuka kita akan langsung ran app.py
   app.run(debug=True)
