48227d79b85ccbc157f28512415e1abae4f3464b
fauzaanirsyaadi <56173199+fauzaanirsyaadi@users.noreply.github.com>
2021-05-19 14:28:55 +0700

    updata code sqlalchemy for db

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 from forms import Todo
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,template_folder='template')
 app.config['SECRET_KEY'] = 'password'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class TodoModel(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String(256))
+
+  def __str__(self):
+    return f'{self.content}, {self.id}'
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def hello_world(): 
   
   request_method = request.method
+  todo = TodoModel.query.all()
+
   if request.method=="POST":
     first_name=request.form('first_name')
     return redirect(url_for('name', first_name=first_name))
-  return render_template('hello.html', request_method=request_method)
+  return render_template('hello.html', request_method=request_method, todo=todo)
 
 @app.route('/name/<string:first_name>')
 def name(first_name):
@@ -21,7 +33,10 @@ def name(first_name):
 def todo():
   todo_form = Todo()
   if todo_form.validate_on_submit():
-    print(todo_form.content.data)
+    todo = TodoModel(content=todo_form.content.data)
+    # print(todo_form.content.data)
+    db.session.add(todo)
+    db.session.commit()
     return redirect('/')
   return render_template('todo.html', form=todo_form)
 
